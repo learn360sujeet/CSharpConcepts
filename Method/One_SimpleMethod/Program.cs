bd279e4d77689b3fda35f9da0f327a7fa4ca3ca9
ASUS <ASUS@B-PROGRAMMER>
2022-12-23 08:02:57 +0530

    Add project files.

---
+++ Method/One_SimpleMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One_SimpleMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Method/Two_Method_VariableParameter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Method_VariableParameter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
